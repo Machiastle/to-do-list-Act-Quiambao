bc57b372d45efc34a5f94ee490316cb60347ca1d
Machiastle <92347210+Machiastle@users.noreply.github.com>
2023-12-19 11:47:17 +0800

    To-do-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import editImage from './Images/edit.png';
+import deleteImage from './Images/delete.png';
 function App() {
+  const [tasks, Tasks] = useState([]);
+  const [taskInput, TaskInput] = useState('');
+  const [editingTaskId, TaskID] = useState(null);
+
+  const AddTask = () => {
+    if (taskInput.trim() !== '') {
+      if (editingTaskId !== null) {
+        const updatedTasks = tasks.map((task) =>
+          task.id === editingTaskId ? { ...task, text: taskInput } : task
+        );
+        Tasks(updatedTasks);
+        TaskID(null);
+      } else {
+        Tasks([...tasks, { id: Date.now(), text: taskInput }]);
+      }
+      TaskInput('');
+    }
+  };
+
+  const DeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    Tasks(updatedTasks);
+  };
+
+  const EditTask = (taskId) => {
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    TaskInput(taskToEdit.text);
+    TaskID(taskId);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='Title'>GAWAIN MO</h1>
+      <div className="input_container">
+        <input
+          type="text"
+          placeholder="Enter a new task"
+          value={taskInput}
+          onChange={(e) => TaskInput(e.target.value)}
+        />
+        <button onClick={AddTask}>
+          {editingTaskId !== null ? 'Update Task' : 'Add Task'}
+        </button>
+      </div>
+      <ul className="task_list">
+        {tasks.map((task) => (
+          <li key={task.id} className="task">
+            <input
+              type="text"
+              value={task.text}
+              onChange={() => {}}
+              disabled={editingTaskId === task.id}
+            />
+            <div>
+              <button onClick={() => EditTask(task.id)}>
+                <img src={editImage} alt='Edit' className='edit' />
+              </button>
+              <button onClick={() => DeleteTask(task.id)}>
+                <img src={deleteImage} alt='Delete' className='delete' />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
